20b3d0537849fb4f44fd2e8dbe85fe177dbd4cf8
anup <anuppal602@gmail.com>
2022-01-22 16:10:04 +0530

    get post

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}   );
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.get('/api/v1/tours',(req,res)=>{
+res.status(200).json({
+    status:'success',
+    results:tours.length,
+    data: {
+        tours
+    }
+});
+});
 
 app.listen(3000, (req,res) => {
     console.log('Server is running on port 3000');

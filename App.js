6543756b98eba7b02d8a5d4b0b48bd2f1dec1a21
anup <anuppal602@gmail.com>
2022-03-02 16:01:53 +0530

    post

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-
-
-const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-app.get('/api/v1/tours',(req,res)=>{
-res.status(200).json({
-    status:'success',
-    results:tours.length,
+app.use(express.json()); //middleware
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
     data: {
-        tours
-    }
-});
+      tours,
+    },
+  });
 });
 
-app.listen(3000, (req,res) => {
-    console.log('Server is running on port 3000');
-} );
+// p
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+            tour: newTour,
+            },
+        });
+    })
+
+});
+app.listen(3000, (req, res) => {
+  console.log('Server is running on port 3000');
+});

4dfb28b8c19ed07160c79930d830948674369cde
anup <anuppal602@gmail.com>
2022-03-06 20:07:04 +0530

    nn

---
+++ files.js
@@ -0,0 +1,11 @@
+// const fs = require('fs');
+// fs.readFile('./docs/blog.txt',(err, data) => {
+//     if(err) throw err;
+//     console.log(data.toString());
+// });
+
+// fs.writeFile('./docs/blog.txt','anupp',(err, data) => {
+//     console.log('firstName');
+// })
+
+console.log('Hello World');
